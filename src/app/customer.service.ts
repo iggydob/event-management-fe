064b173e994c2ca61e7d408debb5bd9a895e8cd9
iggydob <138367360+iggydob@users.noreply.github.com>
2024-05-14 11:21:22 +0300

    Model and Service added

---
+++ src/app/customer.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from "../environments/environment";
+import {Customer} from "./customer";
+
+
+@Injectable({providedIn: 'root'})
+export class CustomerService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${this.apiServerUrl}/api/v1/customers`);
+  }
+
+  public addCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(`${this.apiServerUrl}/api/v1/customers`, customer);
+  }
+
+  public updateCustomer(customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.apiServerUrl}/api/v1/customers`, customer);
+  }
+
+  public deleteCustomer(customerId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/v1/customers/${customerId}`);
+  }
+}
---
+++ src/app/customer.ts
@@ -0,0 +1,8 @@
+export interface Customer {
+  id: number;
+  name: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  tier: number;
+}
